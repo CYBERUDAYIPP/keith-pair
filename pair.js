9d54ae6c3cc9e40b611a98fe7d937149c655bb3e
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-01-07 14:55:28 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('nznyEEkJVfG5Rwb5bvUE5wCmIzFZEyiu')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
